05a3ae5577513418d7926962e232e173d1d94bd1
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2021-11-07 00:31:51 +0100

    custom headers propagated through proxy

--- controller/proxyController.js
+++ controller/proxyController.js
@@ -1,24 +1,37 @@
-// @desc: register new user
-// @route: POST /api/users/register
+// @desc: proxy user request
+// @route: /api/proxy
+// @query: url: string
 
 const axios = require("axios")
-
+const getCustomHeaders = (headers) => {
+  const headersArray = Object.entries(headers)
+    .filter(([key, value]) => {
+      return key.startsWith("mobula-proxy-")
+    })
+    .map(([key, value]) => {
+      return { [key.toString().replace("mobula-proxy-", "")]: value }
+    })
+  let headersObj = {}
+  headersArray.forEach((entry) => {
+    headersObj = { ...headersObj, ...entry }
+  })
+  return headersObj
+}
 // @access: public
 exports.proxyController = async (req, res) => {
   try {
     const axiosConfig = {
       url: req.query.url,
       method: req.method,
-      // distinguish between custom and standard headers, propagate custom only
-      //   headers: req.headers,
+      headers: getCustomHeaders(req.headers),
       params: req.params,
       data: req.data,
     }
     const response = await axios(axiosConfig)
-    // console.log(response.headers)
     const proxyResponse = {
       originalData: response.data,
-      originalHeaders: response.headers,
+      originalResponseHeaders: response.headers,
+      proxyRequestHeaders: response.request._header,
     }
     res.status(response.status).send(proxyResponse)
   } catch (error) {
--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,6 +1,4 @@
 module.exports = (req, res, next) => {
-  console.log("cors triggered")
-
   res.setHeader("Access-Control-Expose-Headers", "*")
   res.setHeader("Access-Control-Allow-Credentials", "true")
   res.setHeader("Access-Control-Max-Age", "3600")
@@ -10,10 +8,5 @@ module.exports = (req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, PUT, POST, PATCH, DELETE, HEAD, OPTIONS"
   )
-  // intercept OPTIONS method
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200)
-  } else {
-    next()
-  }
+  next()
 }
