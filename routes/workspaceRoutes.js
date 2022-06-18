0c8d6e57240e8dd5ec7de18b44bf5a46c8d15dc1
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2022-06-18 22:28:47 +0200

    auth middleware added

--- routes/workspaceRoutes.js
+++ routes/workspaceRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const workspaceController = require('../controller/workspaceController')
+const { auth } = require('../middleware/authMiddleware')
 
 router.route('/').post(workspaceController.createWorkspace)
 router
   .route('/:id')
-  .get(workspaceController.getWorkspaceById)
-  .delete(workspaceController.deleteWorkspace)
-router.route('/getall/:userId').get(workspaceController.getUsersWorkspaces)
+  .get(auth, workspaceController.getWorkspaceById)
+  .delete(auth, workspaceController.deleteWorkspace)
+router
+  .route('/getall/:userId')
+  .get(auth, workspaceController.getUsersWorkspaces)
 
 module.exports = router
